af2d546cf68f5b31e3ed087103abd34ff1053f4f
Elena Pet <dragonqueen3@yandex.ru>
2025-04-18 17:13:10 +0700

    change conftest, add tests in test_burger

--- conftest.py
+++ conftest.py
@@ -3,16 +3,11 @@ from unittest.mock import Mock
 import pytest
 
 @pytest.fixture
-def mock_bun(name, price):
+def mock_bun():
     mock_bun = Mock()
-    mock_bun.name = name
-    mock_bun.price = price
     return mock_bun
 
 @pytest.fixture
-def mock_ingredient(ingredient_type, name, price):
+def mock_ingredient():
     mock_ingredient = Mock()
-    mock_ingredient.type = ingredient_type
-    mock_ingredient.name = name
-    mock_ingredient.price = price
     return mock_ingredient
